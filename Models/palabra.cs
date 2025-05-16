81064ecd76a4e3cc3463aa5419e4c90a7ada63b7
IVAN DANIEL SHOFFER <48861728@ort>
2025-05-16 15:29:42 -0300

    Finalizado models

--- Models/palabra.cs
+++ Models/palabra.cs
@@ -4,22 +4,37 @@ public static class palabra
     public static List<char> intentos{get; private set;} 
     public static bool gano{get; private set;}
     public static bool finalizo{get; private set;}
+      public static string palabraSecreta;
+ 
     public static void nuevaPartida()
     {
         Random random = new Random();
        int numeroAleatorio = random.Next(0, palabraJugable.Count);
-       string palabraSecreta = palabraJugable[numeroAleatorio];
+        palabraSecreta = palabraJugable[numeroAleatorio];
+        intentos = new List<char>(); 
+        gano = false;
+        finalizo = false;
 
 
 
     }
-    public static void encontrarPalabraOculta ( string palabraSecreta)
+    public static string encontrarPalabraOculta ()
     {
         string resultado = "";
     for (int i = 0; i < palabraSecreta.Count(); i++)
     {
-       
+
+            if (intentos.Contains(palabraSecreta[i]))
+            {
+                resultado += palabraSecreta[i];
+               
+            }
+            else
+            {
+                resultado += "_";
+            }          
     }
+    return resultado;
     }
 
     public static void inicializarLetra(char letra)
@@ -29,10 +44,43 @@ public static class palabra
         if(intentos.Contains(letra)==false)
         {
           intentos.Add(letra);
+          verificarGanador();
         }
         
     }
-    
+    public static bool ArriesgarPalabra(string palabraIngresada)
+{
+    palabraIngresada = palabraIngresada.ToUpper();
 
+    if (palabraIngresada == palabraSecreta)
+    {
+        gano = true;
+    }
+    else
+    {
+        gano = false;
+    }
+
+    finalizo = true; 
+    return gano;
 }
+    public static bool verificarGanador()
 
+    {
+        
+      string palabraFinal = encontrarPalabraOculta();
+      
+    if (palabraFinal == palabraSecreta)
+    {
+        gano = true;
+    }
+    else
+    {
+        gano = false;
+    }
+
+    finalizo = true; 
+    return gano;
+}
+    }
+    
