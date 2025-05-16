8e02e6002e3571b6d853eb8b9780fa068a8408ed
GAEL RICARDO MIZRAHI <49191896@ort>
2025-05-16 14:33:36 -0300

    controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,4 +17,16 @@ public class HomeController : Controller
     {
         return View();
     }
+    public IActionResult Juego()
+        {
+            /*La palabra parcialmente descubierta (con guiones bajos en las letras aún no acertadas).
+            Las letras ya utilizadas.*/ 
+            int intentos=0;
+            intentos=palabra.intentos.Count;
+            foreach(char letra in palabra.intentos)
+            Console.WriteLine(letra);
+             return View("Juego");
+        }
+   
 }
+
