d9b97c922224430e549cfb5b0590532aaf4cbed2
GAEL RICARDO MIZRAHI <49191896@ort>
2025-05-16 14:03:21 -0300

    Model

--- Models/palabra.cs
+++ Models/palabra.cs
@@ -1,4 +1,21 @@
 public static class palabra
 {
-    public static string palabraJugable {get; private set;}
+    private static  List<string> palabraJugable = new List<string> {"AHORCADO", "PROGRAMACION", "TECNOLOGIA", "PROFESORA", "ARTE"};
+    public static List<char> intentos{get; private set;} 
+    public static bool gano{get; private set;}
+    public static bool finalizo{get; private set;}
+
+    public static void inicializarLetra(char letra)
+    {
+       
+        letra = char.ToUpper(letra);
+        if(intentos.Contains(letra)==false)
+        {
+          intentos.Add(letra);
+        }
+        
+    }
+    
+
 }
+
